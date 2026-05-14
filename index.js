3acf060bfefdd8a42981faee1a6649de1cf1c451
uwi112211-cyber <uwi112211@gmail.com>
2026-05-15 01:43:43 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.MessageContent
+    ] 
+});
+
+client.once('ready', () => {
+    console.log(`✅ تم تشغيل البوت بنجاح باسم: ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+    if (message.content === 'ping') {
+        message.reply('pong! 🏓');
+    }
+});
+
+client.login(process.env.TOKEN);
